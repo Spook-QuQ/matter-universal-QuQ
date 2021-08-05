d59cd0c6dfaed2f070d0aa5a1a57446a9ec8d640
OwO <OwO.futon@gmail.com>
2021-08-05 17:33:55 +0900

    code type changed to ESM type

--- MatterObject.js
+++ MatterObject.js
@@ -1,125 +1,111 @@
-(_=> {
+export default class MatterObjectQuQ {
+  static Matter
 
-  const moduleName = 'MatterObject'
+  static setMatter (_Matter) {
+    this.Matter = _Matter
+  }
 
-  const MatterObjectQuQ = class MatterObjectQuQ {
-    static Matter
+  // constructor (ctx, _Matter = Matter) {
+  //   this.Matter = _Matter
 
-    static setMatter (_Matter) {
-      this.Matter = _Matter
-    }
+  constructor (ctx) {
+    this.Matter = MatterObjectQuQ.Matter
 
-    // constructor (ctx, _Matter = Matter) {
-    //   this.Matter = _Matter
+    // const {
+    //   Bodies,
+    // } = _Matter
 
-    constructor (ctx) {
-      this.Matter = MatterObjectQuQ.Matter
+    const {
+      Bodies,
+      Constraint
+    } = this.Matter
 
-      // const {
-      //   Bodies,
-      // } = _Matter
+    const {
+      id,
+      type,
+      variables // array
+    } = ctx
 
-      const {
-        Bodies,
-        Constraint
-      } = this.Matter
-
-      const {
-        id,
-        type,
-        variables // array
-      } = ctx
-
-      if (type === 'constraint') {
-        this.body = Constraint.create({...variables})
-        this.body.OwO = this
-        this.bodies = {
-          bodyA: variables.bodyA,
-          bodyB: variables.bodyB
-        }
-      } else {
-        this.body = Bodies[type](...variables)
-        this.body.OwO = this
-        this.currentScale = { x: 1, y: 1 }
+    if (type === 'constraint') {
+      this.body = Constraint.create({...variables})
+      this.body.OwO = this
+      this.bodies = {
+        bodyA: variables.bodyA,
+        bodyB: variables.bodyB
       }
-
-      if (id) this.body.id = id
-
-      this.defaultVariables = variables
-
-      this.type = type
+    } else {
+      this.body = Bodies[type](...variables)
+      this.body.OwO = this
+      this.currentScale = { x: 1, y: 1 }
     }
 
-    add () {
-      // if (!this.engine) return
-      // this.Matter.Composite.add(this.engine.world, this.body)
-      // return this
-      console.log('Not added to any world yet')
-    }
+    if (id) this.body.id = id
 
-    remove () {
-      // if (!this.engine) return
-      // this.Matter.World.remove(this.engine.world, this.body)
-      // return this
-      console.log('Not added to any world yet')
-    }
+    this.defaultVariables = variables
 
-    rotate (rotation) {
-      this.Matter.Body.rotate(this.body, rotation)
-      return this
-    }
-
-    setAngle (angle) {
-      this.Matter.Body.setAngle(this.body, angle)
-      return this
-    }
-
-    setAngularVelocity (velocity) {
-      this.Matter.Body.setAngularVelocity(this.body, velocity)
-      return this
-    }
-
-    setInertia (inertia) {
-      this.Matter.Body.setInertia(this.body, inertia)
-      return this
-    }
-
-    translate (translation) {
-      this.Matter.Body.translate(this.body, translation)
-      return this
-    }
-
-    setPosition(position) {
-      this.Matter.Body.setPosition(this.body, position)
-      return this
-    }
-
-    setVelocity (velocity) {
-      this.Matter.Body.setVelocity(this.body, velocity)
-      return this
-    }
-
-    scale (ctx) {
-      const { x, y } = ctx
-      this.Matter.Body.scale(this.body, x, y)
-      this.currentScale.x *= x
-      this.currentScale.y *= y
-      return this
-    }
-
-    setStatic (isStatic) {
-      this.Matter.Body.setStatic(this.body, isStatic)
-      return this
-    }
-
+    this.type = type
   }
 
-  // ==
-
-  if (typeof module === "object" && typeof module.exports === "object") {
-    module.exports = MatterObjectQuQ
-  } else {
-    this[moduleName] = MatterObjectQuQ
+  add () {
+    // if (!this.engine) return
+    // this.Matter.Composite.add(this.engine.world, this.body)
+    // return this
+    console.log('Not added to any world yet')
   }
 
-})``
+  remove () {
+    // if (!this.engine) return
+    // this.Matter.World.remove(this.engine.world, this.body)
+    // return this
+    console.log('Not added to any world yet')
+  }
+
+  rotate (rotation) {
+    this.Matter.Body.rotate(this.body, rotation)
+    return this
+  }
+
+  setAngle (angle) {
+    this.Matter.Body.setAngle(this.body, angle)
+    return this
+  }
+
+  setAngularVelocity (velocity) {
+    this.Matter.Body.setAngularVelocity(this.body, velocity)
+    return this
+  }
+
+  setInertia (inertia) {
+    this.Matter.Body.setInertia(this.body, inertia)
+    return this
+  }
+
+  translate (translation) {
+    this.Matter.Body.translate(this.body, translation)
+    return this
+  }
+
+  setPosition(position) {
+    this.Matter.Body.setPosition(this.body, position)
+    return this
+  }
+
+  setVelocity (velocity) {
+    this.Matter.Body.setVelocity(this.body, velocity)
+    return this
+  }
+
+  scale (ctx) {
+    const { x, y } = ctx
+    this.Matter.Body.scale(this.body, x, y)
+    this.currentScale.x *= x
+    this.currentScale.y *= y
+    return this
+  }
+
+  setStatic (isStatic) {
+    this.Matter.Body.setStatic(this.body, isStatic)
+    return this
+  }
+
+}
--- MatterWorld.js
+++ MatterWorld.js
@@ -1,287 +1,273 @@
-(_=> {
+export default class MatterWorldQuQ {
+  static Matter
+  static MatterObject
 
-  const moduleName = 'MatterWorld'
+  static setMatter (_Matter) {
+    this.Matter = _Matter
+  }
+  static setMatterObject (_MatterObject) {
+    this.MatterObject = _MatterObject
+  }
 
-  const MatterWorldQuQ = class MatterWorldQuQ {
-    static Matter
-    static MatterObject
+  // constructor (_Matter = Matter, _MatterObject = MatterObject) {
+  //   this.Matter = _Matter
+  //   this.MatterObject = _MatterObject
+  constructor () {
 
-    static setMatter (_Matter) {
-      this.Matter = _Matter
-    }
-    static setMatterObject (_MatterObject) {
-      this.MatterObject = _MatterObject
-    }
+    this.Matter = MatterWorldQuQ.Matter
+    this.MatterObject = MatterWorldQuQ.MatterObject
 
-    // constructor (_Matter = Matter, _MatterObject = MatterObject) {
-    //   this.Matter = _Matter
-    //   this.MatterObject = _MatterObject
-    constructor () {
+    // const {
+    //   Engine,
+    //   Runner
+    // } = _Matter
+    const {
+      Engine,
+      Runner
+    } = this.Matter
 
-      this.Matter = MatterWorldQuQ.Matter
-      this.MatterObject = MatterWorldQuQ.MatterObject
+    const engine = Engine.create()
+    const runner = Runner.create()
+    Runner.run(runner, engine)
 
-      // const {
-      //   Engine,
-      //   Runner
-      // } = _Matter
-      const {
-        Engine,
-        Runner
-      } = this.Matter
+    this.engine = engine
+    this.runner = runner
 
-      const engine = Engine.create()
-      const runner = Runner.create()
-      Runner.run(runner, engine)
+    this.canvasMouse = null
+    this.mouseConstraint = null
+    this.mouseEvents = null
+    this.isMouseEventsOn = null
 
-      this.engine = engine
-      this.runner = runner
+    // this.currentObjectIndex = 0
+    this.activeObjects = {}
+  }
 
-      this.canvasMouse = null
-      this.mouseConstraint = null
-      this.mouseEvents = null
-      this.isMouseEventsOn = null
-
-      // this.currentObjectIndex = 0
-      this.activeObjects = {}
-    }
-
-    _ifMatterObjectOwO (obj) {
-      let rsObject = null
-      if (obj.constructor === this.MatterObject) {
-        // obj.engine = this.engine
-        rsObject = obj.body
-        obj.add = () => this.add(obj)
-        obj.remove = () => this.remove(obj)
-      } else {
-        rsObject = obj
-      }
-
-      // const index = this.currentObjectIndex += 1 // 加算後の値が代入されている
-      // this.activeObjects[index] = rsObject
-      this.activeObjects[rsObject.id] = rsObject
-      // rsObject._objectIndex = index
-      return rsObject
+  _ifMatterObjectOwO (obj) {
+    let rsObject = null
+    if (obj.constructor === this.MatterObject) {
+      // obj.engine = this.engine
+      rsObject = obj.body
+      obj.add = () => this.add(obj)
+      obj.remove = () => this.remove(obj)
+    } else {
+      rsObject = obj
     }
 
-    add (object) {
+    // const index = this.currentObjectIndex += 1 // 加算後の値が代入されている
+    // this.activeObjects[index] = rsObject
+    this.activeObjects[rsObject.id] = rsObject
+    // rsObject._objectIndex = index
+    return rsObject
+  }
 
-      if (Array.isArray(object)) { // 配列の場合
-        const rsObjects = object.map(obj => this._ifMatterObjectOwO(obj))
-        this.Matter.Composite.add(this.engine.world, rsObjects)
+  add (object) {
 
-      } else { // 配列ではない場合
-        let rsObject = this._ifMatterObjectOwO(object)
-        this.Matter.Composite.add(this.engine.world, rsObject)
+    if (Array.isArray(object)) { // 配列の場合
+      const rsObjects = object.map(obj => this._ifMatterObjectOwO(obj))
+      this.Matter.Composite.add(this.engine.world, rsObjects)
 
-      }
+    } else { // 配列ではない場合
+      let rsObject = this._ifMatterObjectOwO(object)
+      this.Matter.Composite.add(this.engine.world, rsObject)
+
     }
+  }
 
-    remove (object) {
-      const rsObject = object.constructor === this.MatterObject
-       ? object.body
-       : object
+  remove (object) {
+    const rsObject = object.constructor === this.MatterObject
+     ? object.body
+     : object
 
-      // delete this.activeObjects[rsObject._objectIndex]
-      delete this.activeObjects[rsObject.id]
-      this.Matter.World.remove(this.engine.world, rsObject)
-    }
+    // delete this.activeObjects[rsObject._objectIndex]
+    delete this.activeObjects[rsObject.id]
+    this.Matter.World.remove(this.engine.world, rsObject)
+  }
 
-    makeRenderer (element) {
-      this.renderer = this.Matter.Render.create({
-          element,
-          engine: this.engine
+  makeRenderer (element) {
+    this.renderer = this.Matter.Render.create({
+        element,
+        engine: this.engine
+    })
+    return this
+  }
+
+  runRenderer () {
+    this.Matter.Render.run(this.renderer)
+  }
+
+
+  // ======== ======== ======== ======== ======== ======== ========
+  // ======== ======== ======== ======== ======== ======== ========
+  // ======== ======== ======== ======== ======== ======== ========
+
+
+  addMouseEvent (ctx) { // Pixiと合わせて使う場合は使用する必要が無い
+    if (!this.canvasMouse && !this.mouseConstraint) {
+      this.canvasMouse = this.Matter.Mouse.create(document.querySelector('canvas'))
+      this.mouseConstraint = this.Matter.MouseConstraint.create(this.engine, {
+        mouse: this.canvasMouse
       })
-      return this
+      this.mouseEvents = {}
+      this.isMouseEventsOn = {}
     }
 
-    runRenderer () {
-      this.Matter.Render.run(this.renderer)
+    const {
+      name,
+      type,
+      handler
+    } = ctx
+
+    if (!this.isMouseEventsOn[type]) {
+      this.isMouseEventsOn[type] = true
+      this.mouseEvents[type] = {}
+      this.Matter.Events.on(this.mouseConstraint, type, e => {
+        // this.mouseEvents[type].forEach(cb => cb(e))
+        Object.values(this.mouseEvents[type]).forEach(cb => cb(e))
+      })
     }
 
+    // this.mouseEvents[type].push(handler)
+    this.mouseEvents[type][name] = handler
+  }
 
-    // ======== ======== ======== ======== ======== ======== ========
-    // ======== ======== ======== ======== ======== ======== ========
-    // ======== ======== ======== ======== ======== ======== ========
+  removeMouseEvent (ctx) {
+    const {
+      name,
+      type
+    } = ctx
 
-
-    addMouseEvent (ctx) { // Pixiと合わせて使う場合は使用する必要が無い
-      if (!this.canvasMouse && !this.mouseConstraint) {
-        this.canvasMouse = this.Matter.Mouse.create(document.querySelector('canvas'))
-        this.mouseConstraint = this.Matter.MouseConstraint.create(this.engine, {
-          mouse: this.canvasMouse
-        })
-        this.mouseEvents = {}
-        this.isMouseEventsOn = {}
-      }
-
-      const {
-        name,
-        type,
-        handler
-      } = ctx
-
-      if (!this.isMouseEventsOn[type]) {
-        this.isMouseEventsOn[type] = true
-        this.mouseEvents[type] = {}
-        this.Matter.Events.on(this.mouseConstraint, type, e => {
-          // this.mouseEvents[type].forEach(cb => cb(e))
-          Object.values(this.mouseEvents[type]).forEach(cb => cb(e))
-        })
-      }
-
-      // this.mouseEvents[type].push(handler)
-      this.mouseEvents[type][name] = handler
+    if (this.mouseEvents[type] && this.mouseEvents[type][name]) {
+      delete this.mouseEvents[type][name]
     }
+  }
 
-    removeMouseEvent (ctx) {
-      const {
-        name,
-        type
-      } = ctx
+  get state () {
+    return {
+      // currentObjectIndex: this.currentObjectIndex,
+      activeObjects: Object.keys(this.activeObjects).reduce((list, id) => {
+        const {
+          angle,
+          angularVelocity,
+          label,
+          position,
+          velocity,
+        } = this.activeObjects[id]
 
-      if (this.mouseEvents[type] && this.mouseEvents[type][name]) {
-        delete this.mouseEvents[type][name]
-      }
-    }
+        if (this.activeObjects[id].OwO?.type === 'constraint') {
+          const defaultVariables = this.activeObjects[id].OwO?.defaultVariables
+          delete defaultVariables.bodyA
+          delete defaultVariables.bodyB
 
-    get state () {
-      return {
-        // currentObjectIndex: this.currentObjectIndex,
-        activeObjects: Object.keys(this.activeObjects).reduce((list, id) => {
-          const {
-            angle,
-            angularVelocity,
-            label,
-            position,
-            velocity,
-          } = this.activeObjects[id]
-
-          if (this.activeObjects[id].OwO?.type === 'constraint') {
-            const defaultVariables = this.activeObjects[id].OwO?.defaultVariables
-            delete defaultVariables.bodyA
-            delete defaultVariables.bodyB
-
-            list[id] = {
-              id,
-              type: this.activeObjects[id].OwO?.type,
-              bodyIds: {
-                bodyA: this.activeObjects[id].OwO?.bodies.bodyA.id,
-                bodyB: this.activeObjects[id].OwO?.bodies.bodyB.id,
-              },
-              defaultVariables
-            }
-            return list
-          } else {
-            list[id] = {
-              id,
-              state: {
-                angle,
-                angularVelocity,
-                position: {...position || {}},
-                velocity: {...velocity || {}},
-              },
-              currentScale: this.activeObjects[id].OwO?.currentScale,
-              type: this.activeObjects[id].OwO?.type,
-              defaultVariables: this.activeObjects[id].OwO?.defaultVariables
-            }
+          list[id] = {
+            id,
+            type: this.activeObjects[id].OwO?.type,
+            bodyIds: {
+              bodyA: this.activeObjects[id].OwO?.bodies.bodyA.id,
+              bodyB: this.activeObjects[id].OwO?.bodies.bodyB.id,
+            },
+            defaultVariables
           }
           return list
-        }, {})
-      }
-    }
-
-    _updateStateOfObject (nextObject, target) {
-      if (!nextObject.state) return // constraint は state が無いのでここで終了
-
-      if (target && target.OwO) {
-        Object.keys(nextObject.state).forEach(property => {
-          switch (property) {
-            case 'angle': return target.OwO.setAngle(nextObject.state[property])
-            case 'angularVelocity': return target.OwO.setAngularVelocity(nextObject.state[property])
-            case 'position': return target.OwO.setPosition(nextObject.state[property])
-            case 'velocity': return target.OwO.setVelocity(nextObject.state[property])
-          }
-        })
-      } else if (target) {
-        Object.keys(nextObject.state).forEach(property => {
-          const { Body } = this.Matter
-          switch (property) {
-            case 'angle': return Body.setAngle(target, nextObject.state[property])
-            case 'angularVelocity': return Body.setAngularVelocity(target, nextObject.state[property])
-            case 'position': return Body.setPosition(target, nextObject.state[property])
-            case 'velocity': return Body.setVelocity(target, nextObject.state[property])
-          }
-        })
-      }
-    }
-
-    set state (_state = {}) {
-      const {
-        // currentObjectIndex,
-        activeObjects: nextObjects
-      } = _state
-
-      // if (!currentObjectIndex) return
-      if (!nextObjects) return
-
-      // this.currentObjectIndex = currentObjectIndex
-      const keysOfNextObjects = Object.keys(nextObjects)
-      const keysOfPrevObjects = Object.keys(this.activeObjects)
-
-      // next の state に存在しないオブジェクトを prev から削除
-      keysOfPrevObjects
-        .filter(key => !keysOfNextObjects.includes(key))
-        .forEach(deleteTargetKey => {
-          this.remove(this.activeObjects[deleteTargetKey])
-        })
-
-      // 存在しているオブジェクトの state を更新
-      keysOfNextObjects.forEach(key => {
-        const target = this.activeObjects[key]
-
-        // 現在のstateに、更新対象のオブジェクトが存在しない場合に追加する
-        if (!target) {
-          if (nextObjects[key].type === 'constraint') {
-            const bodyIds = nextObjects[key].bodyIds
-            if (!this.activeObjects[bodyIds.bodyA] || !this.activeObjects[bodyIds.bodyB]) return
-            console.log(nextObjects[key].defaultVariables);
-            const newObject = new this.MatterObject({
-              id: nextObjects[key].id,
-              type: nextObjects[key].type,
-              variables: {
-                bodyA: this.activeObjects[bodyIds.bodyA],
-                bodyB: this.activeObjects[bodyIds.bodyB],
-                ...(nextObjects[key].defaultVariables || {})
-              }
-            })
-
-            this.add(newObject)
-          } else if (nextObjects[key].defaultVariables) {
-            const newObject = new this.MatterObject({
-              id: nextObjects[key].id,
-              type: nextObjects[key].type,
-              variables: nextObjects[key].defaultVariables || {}
-            })
-
-            this.add(newObject)
-            // update するのは body だけなので
-            this._updateStateOfObject(nextObjects[key], newObject.body)
+        } else {
+          list[id] = {
+            id,
+            state: {
+              angle,
+              angularVelocity,
+              position: {...position || {}},
+              velocity: {...velocity || {}},
+            },
+            currentScale: this.activeObjects[id].OwO?.currentScale,
+            type: this.activeObjects[id].OwO?.type,
+            defaultVariables: this.activeObjects[id].OwO?.defaultVariables
           }
         }
+        return list
+      }, {})
+    }
+  }
 
-        if (target?.type === 'constraint') return
-        this._updateStateOfObject(nextObjects[key], target)
+  _updateStateOfObject (nextObject, target) {
+    if (!nextObject.state) return // constraint は state が無いのでここで終了
+
+    if (target && target.OwO) {
+      Object.keys(nextObject.state).forEach(property => {
+        switch (property) {
+          case 'angle': return target.OwO.setAngle(nextObject.state[property])
+          case 'angularVelocity': return target.OwO.setAngularVelocity(nextObject.state[property])
+          case 'position': return target.OwO.setPosition(nextObject.state[property])
+          case 'velocity': return target.OwO.setVelocity(nextObject.state[property])
+        }
+      })
+    } else if (target) {
+      Object.keys(nextObject.state).forEach(property => {
+        const { Body } = this.Matter
+        switch (property) {
+          case 'angle': return Body.setAngle(target, nextObject.state[property])
+          case 'angularVelocity': return Body.setAngularVelocity(target, nextObject.state[property])
+          case 'position': return Body.setPosition(target, nextObject.state[property])
+          case 'velocity': return Body.setVelocity(target, nextObject.state[property])
+        }
       })
     }
   }
 
-  // ==
+  set state (_state = {}) {
+    const {
+      // currentObjectIndex,
+      activeObjects: nextObjects
+    } = _state
 
-  if (typeof module === "object" && typeof module.exports === "object") {
-    module.exports = MatterWorldQuQ
-  } else {
-    this[moduleName] = MatterWorldQuQ
+    // if (!currentObjectIndex) return
+    if (!nextObjects) return
+
+    // this.currentObjectIndex = currentObjectIndex
+    const keysOfNextObjects = Object.keys(nextObjects)
+    const keysOfPrevObjects = Object.keys(this.activeObjects)
+
+    // next の state に存在しないオブジェクトを prev から削除
+    keysOfPrevObjects
+      .filter(key => !keysOfNextObjects.includes(key))
+      .forEach(deleteTargetKey => {
+        this.remove(this.activeObjects[deleteTargetKey])
+      })
+
+    // 存在しているオブジェクトの state を更新
+    keysOfNextObjects.forEach(key => {
+      const target = this.activeObjects[key]
+
+      // 現在のstateに、更新対象のオブジェクトが存在しない場合に追加する
+      if (!target) {
+        if (nextObjects[key].type === 'constraint') {
+          const bodyIds = nextObjects[key].bodyIds
+          if (!this.activeObjects[bodyIds.bodyA] || !this.activeObjects[bodyIds.bodyB]) return
+          console.log(nextObjects[key].defaultVariables);
+          const newObject = new this.MatterObject({
+            id: nextObjects[key].id,
+            type: nextObjects[key].type,
+            variables: {
+              bodyA: this.activeObjects[bodyIds.bodyA],
+              bodyB: this.activeObjects[bodyIds.bodyB],
+              ...(nextObjects[key].defaultVariables || {})
+            }
+          })
+
+          this.add(newObject)
+        } else if (nextObjects[key].defaultVariables) {
+          const newObject = new this.MatterObject({
+            id: nextObjects[key].id,
+            type: nextObjects[key].type,
+            variables: nextObjects[key].defaultVariables || {}
+          })
+
+          this.add(newObject)
+          // update するのは body だけなので
+          this._updateStateOfObject(nextObjects[key], newObject.body)
+        }
+      }
+
+      if (target?.type === 'constraint') return
+      this._updateStateOfObject(nextObjects[key], target)
+    })
   }
-
-})``
+}
